e0060d07dd9eef50f52f0b8123d8072cb87b98d6
cy63r-4lph4 <cy63r.4lph4@gmail.com>
2025-10-31 00:46:17 +0000

    chore: added student courses function to the courseController

--- Backend/app/Http/Controllers/CourseController.php
+++ Backend/app/Http/Controllers/CourseController.php
@@ -5,6 +5,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Course;
+use App\Models\CourseRegistration;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
@@ -30,5 +31,32 @@ class CourseController extends Controller
 
         return response()->json($course, 201);
     }
-    
+
+    public function studentCourses(Request $request)
+    {
+        $student = $request->user();
+
+        $courses = CourseRegistration::with('event')
+            ->where('student_id', $student->id)
+            ->get()
+            ->map(function ($registration) {
+                $event = $registration->event;
+                return [
+                    'id' => $event->id,
+                    'code' => $event->title, // course code
+                    'description' => $event->description,
+                    'type' => $event->type,
+                    'start_time' => $event->start_time,
+                    'end_time' => $event->end_time,
+                    'date' => $event->date,
+                    'streams' => $event->streams,
+                ];
+            });
+
+        return response()->json([
+            'student' => $student->id,
+            'courses' => $courses,
+        ]);
+    }
+
 }
